91c2e58df6c83d8c4493cf42c524a4d157c64a54
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2021-12-17 11:18:02 +0800

    fixed another bug
    a bug that didnt let it spam

--- keyboardspam/keyboardspam/keyboardspam.py
+++ keyboardspam/keyboardspam/keyboardspam.py
@@ -2,10 +2,9 @@ import time
 from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
-
+x = input('type what you want to spam')
+time.sleep(5)
 def spam():
-    x = input('type what you want to spam')
-    time.sleep(5)
     keyboard.type(x)
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
